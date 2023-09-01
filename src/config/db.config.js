2e80992e8b6ded4355ede367aa22012c9ab62e69
Hill-Man-1 <hilmaaansf@gmail.com>
2023-09-01 23:50:32 +0700

    arrggghhhhh

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,9 @@
 const DBConfig = {
-    DB_URL: process.env.DB_RAILWAY_URL,
-    DB_NAME: process.env.DB_RAILWAY_NAME,
-    HOST: process.env.DB_RAILWAY_HOST,
-    PASSWORD: process.env.DB_RAILWAY_PASSWORD,
-    USER: process.env.DB_RAILWAY_USERNAME,
-    PORT: process.env.DB_RAILWAY_PORT
+    HOST: process.env.DB_RAILWAY_HOST || 'containers-us-west-37.railway.app',
+    USER: process.env.DB_RAILWAY_USER || 'root',
+    PASSWORD: process.env.DB_RAILWAY_PASSWORD || '8WiXCEM1TfaJKPKPYbnW',
+    PORT: process.env.DB_RAILWAY_PORT ? parseInt(process.env.DB_RAILWAY_PORT) : 8051,
+    DB_RAILWAY_NAME: process.env.DB_RAILWAY_NAME || 'railway'
 };
 
 module.exports = DBConfig;
