aff5d1c97e342df616489dd7a162e1d2e0d4891e
Hill-Man-1 <hilmaaansf@gmail.com>
2023-09-01 21:11:05 +0700

    update swagger

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const librarianRouter = require ("./routes/librarian-routes");
 const userRouter = require ("./routes/user-routes");
 const { Redis }= require ("ioredis");
 const { JWT_SIGN }= require ("./config/jwt"); 
-const OpenApiValidator= require ('express-openapi-validator');
+// const OpenApiValidator= require ('express-openapi-validator');
 const yaml= require ('yaml');
 const  swaggerUi = require ('swagger-ui-express');
 const fs = require('fs');
@@ -19,10 +19,10 @@ const app = express();
 const port = process.env.URL || 6592;
 
 app.use(bodyParser.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(require('fs').readFileSync('./doc/openapi.yaml', 'utf8'))))
-app.use(OpenApiValidator.middleware({ 
-    apiSpec: './doc/openapi.yaml'
-}))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(fs.readFileSync('./doc/openapi.yaml', 'utf8'))))
+// app.use(OpenApiValidator.middleware({ 
+//     apiSpec: 'doc/openapi.yaml'
+// }))
 
 app.use('/auth', authRouter);
 app.use('/admin', adminRouter);
